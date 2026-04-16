7d953de61b70570ff7136a2491a9197e4c18e853
FaisalKhalid67 <alnafganf1@gmail.com>
2026-04-16 21:28:11 +0300

    Lab7 Work

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path("html5/listing", views.html5_listing, name="html5_listing"),
     path("html5/tables", views.html5_tables, name="html5_tables"),
     path("search/", views.search, name="books.search"),
+    path("simple/query", views.simple_query, name="books.simple_query"),
+    path("complex/query", views.complex_query, name="books.complex_query"),
 ]
